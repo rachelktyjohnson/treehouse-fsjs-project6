3f32e88260890f58fe1e2c960ec9dcbcad255ad4
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-10-23 18:35:35 +1100

    Clean pugs of starter comments

--- app.js
+++ app.js
@@ -3,26 +3,28 @@ const app = express()
 const port = 3000
 const {projects} = require('./data.json');
 
-
 app.set('view engine','pug');
 app.use(express.static('public'));
+
+
+//ROUTES
 
 app.get('/', (req, res) => {
     res.render('index', {projects})
 })
-
 app.get('/about', (req,res)=>{
     res.render('about');
 })
-
 app.get('/projects', (req,res)=>{
     res.render('index');
 })
-
 app.get('/projects/:id', (req,res)=>{
     let projectID = req.params.id;
     res.render('project', {project: projects[projectID]});
 })
+
+
+//LISTENER
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
