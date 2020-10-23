0a81455a7f2b17fc5afb0bd40de6acc1e35fef44
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-10-23 18:16:47 +1100

    Add all data and hook up to landing page and project pages

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {projects} = require('./data.json');
+
 
 app.set('view engine','pug');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {projects})
 })
 
 app.get('/about', (req,res)=>{
     res.render('about');
+})
+
+app.get('/projects', (req,res)=>{
+    res.render('index');
+})
+
+app.get('/projects/:id', (req,res)=>{
+    let projectID = req.params.id;
+    res.render('project', {project: projects[projectID]});
 })
 
 app.listen(port, () => {
