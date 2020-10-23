08c9f8babbf1e229fd316768b1549212ede78725
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-10-23 16:58:49 +1100

    Add all project images

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','pug');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
